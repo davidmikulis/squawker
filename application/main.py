0bcb59f012f037abd24af7da7deabf38e2f22075
davidmikulis <davidmikulis@gmail.com>
2018-07-26 17:11:59 -0600

    Remove Flask-Scss extension, replace with Node-Sass. Set up Scss folders

--- application/main.py
+++ application/main.py
@@ -1,23 +1,8 @@
 # Basic Flask
 from flask import Flask
 
-# Use Scss for styling
-from flask_scss import Scss
-
 # MySQL integration
 # from flask_mysqldb import MySQL
-
-# Read .env files
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
-# Load environment variables and set configuration constants
-CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
-CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
-CALLBACK_URL = "http://127.0.0.1:5000/verify"
 
 # Initialize application
 app = Flask(__name__)
@@ -26,14 +11,8 @@ from oauth import *
 # Load application configuration
 app.config.from_pyfile('config.cfg')
 
-# Initialize Scss
-Scss(app, static_dir='static', asset_dir='assets')
-
 # Initialize MySQL
 # mysql = MySQL(app)
-
-
- 
  
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
--- application/timeline.py
+++ application/timeline.py
@@ -1,9 +1,12 @@
 from main import app
 from flask import request, redirect, url_for, session, g, flash, render_template
-import re
 
 # Library to assist with Twitter APIs
 import tweepy
+
+# Date and time libraries
+from datetime import datetime
+
 
 class Timeline():
     def datamine_media(self, media):
@@ -13,27 +16,28 @@ class Timeline():
             return own_url, media_url
         return None, None
 
-    def generate_re(self):
-        hashtag_re = re.compile(r'#([\w]+)')
-        mention_re = re.compile(r'@([\w]+)')
-        return hashtag_re, mention_re
-
-    def process_text(self, text, hashtag_re, mention_re, url_list, own_url):
-        processed_text = self.sub_hashtag_mention(text, hashtag_re, mention_re)
+    def process_text(self, text, hashtags_list, mentions_list, url_list, own_url):
+        processed_text = text
+        processed_text = self.replace_hashtags(processed_text, hashtags_list)
+        processed_text = self.replace_mentions(processed_text, mentions_list)
         processed_text = self.replace_urls(processed_text, url_list)
         processed_text = self.remove_own_url(processed_text, own_url)
         return processed_text
 
-    def sub_hashtag_mention(self, text, hashtag_re, mention_re):
-        hashtag_text = hashtag_re.sub(r'<a href="https://twitter.com/hashtag/\1" target="_blank">#\1</a>', text)
-        hashtag_mention_text = mention_re.sub(r'<a href="https://twitter.com/\1" target="_blank">@\1</a>', hashtag_text)
-        return hashtag_mention_text
+    def replace_hashtags(self, text, hashtags_list):
+        for hashtag in hashtags_list:
+            text = text.replace('#' + hashtag['text'], '<a href="https://twitter.com/hashtag/{h}" target="_blank">#{h}</a>'.format(h=hashtag['text']))
+        return text
+
+    def replace_mentions(self, text, mentions_list):
+        for mention in mentions_list:
+            text = text.replace('@' + mention['screen_name'], '<a href="https://twitter.com/{m}" target="_blank">@{m}</a>'.format(m=mention['screen_name']))
+        return text
 
     def replace_urls(self, text, url_list):
-        url_text = text
         for url in url_list:
-            url_text = url_text.replace(url['url'], '<a href="{}" target="_blank">{}</a>'.format(url['url'], url['display_url']))
-        return url_text
+            text = text.replace(url['url'], '<a href="{}" target="_blank">{}</a>'.format(url['url'], url['display_url']))
+        return text
 
     def remove_own_url(self, text, own_url):
         if own_url is not None:
@@ -41,6 +45,11 @@ class Timeline():
         else:
             return text
 
+    def time_stamp_str(self, created_at):
+        print(str(created_at))
+        # processed_time = datetime.strptime(str(created_at),'%a %b %d %H:%M:%S +0000 %Y')
+        # print(processed_time, flush=True)
+        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), flush=True)
 
 # Timeline
 @app.route('/')
@@ -56,7 +65,6 @@ def timeline():
         api = tweepy.API(auth)
         raw_tweets = api.home_timeline(tweet_mode='extended')
         tl = Timeline()
-        hashtag_re, mention_re = tl.generate_re()
         for tweet in raw_tweets:
             if hasattr(tweet, 'retweeted_status'):
                 status = tweet.retweeted_status
@@ -67,9 +75,11 @@ def timeline():
             else:
                 status = tweet
                 tweet_type = 'tweet'
-            url_list = status.entities.get('urls', None)
+            url_list = status.entities.get('urls', [])
+            hashtags_list = status.entities.get('hashtags', [])
+            mentions_list = status.entities.get('user_mentions', [])
             own_url, media_url = tl.datamine_media(status.entities.get('media', None))
-            processed_text = tl.process_text(status.full_text, hashtag_re, mention_re, url_list, own_url)
+            processed_text = tl.process_text(status.full_text, hashtags_list, mentions_list, url_list, own_url)
             # Assigned mined values to tweet object
             tweet.text_list = processed_text.splitlines()
             tweet.media_url = media_url
@@ -77,6 +87,7 @@ def timeline():
             processed_tweets.append(tweet)
     return render_template('timeline.html', tweets=processed_tweets, logged_in=logged_in)
 
+# Testing
 # def timeline():
 #     logged_in = False
 #     auth = tweepy.OAuthHandler(app.config['CONSUMER_KEY'], app.config['CONSUMER_SECRET'])
@@ -88,7 +99,6 @@ def timeline():
 #         api = tweepy.API(auth)
 #         tweet = api.get_status('1022555951801487362', tweet_mode='extended')
 #         tl = Timeline()
-#         hashtag_re, mention_re = tl.generate_re()
 #         if hasattr(tweet, 'retweeted_status'):
 #             status = tweet.retweeted_status
 #             tweet_type = 'retweet'
@@ -99,13 +109,16 @@ def timeline():
 #             status = tweet
 #             tweet_type = 'tweet'
 #         url_list = status.entities.get('urls', None)
+#         hashtags_list = status.entities.get('hashtags', None)
+#         mentions_list = status.entities.get('user_mentions', None)
 #         own_url, media_url = tl.datamine_media(status.entities.get('media', None))
-        
-#         processed_text = tl.process_text(status.full_text, hashtag_re, mention_re, url_list, own_url)
+#         print(status.full_text, flush=True)
+#         processed_text = tl.process_text(status.full_text, hashtags_list, mentions_list, url_list, own_url)
 #         # Assigned mined/processed values to tweet object
 #         tweet.text_list = processed_text.splitlines()
 #         tweet.media_url = media_url
 #         tweet.tweet_type = tweet_type
+#         tweet.time_stamp_str = tl.time_stamp_str(tweet.created_at)
 
 #         return render_template('timeline_test.html', 
 #             tweet=tweet, 
